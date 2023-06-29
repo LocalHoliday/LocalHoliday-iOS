7664bf647b8a0f5b05d8c59542977b3a84a567e5
chongin12 <chongin12@naver.com>
2023-06-30 03:29:28 +0900

    add Assets

---
+++ LocalHoliday/LocalHoliday/Extension/Font+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  Text+Extension.swift
+//  LocalHoliday
+//
+//  Created by 정종인 on 2023/06/30.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static let H1B = Font.custom("SUIT-Bold", size: 30)
+    static let H2B = Font.custom("SUIT-Bold", size: 22)
+    static let H3B = Font.custom("SUIT-Bold", size: 18)
+    static let H4B = Font.custom("SUIT-Bold", size: 16)
+
+    static let H2SB = Font.custom("SUIT-SemiBold", size: 22)
+    static let H3SB = Font.custom("SUIT-SemiBold", size: 18)
+    static let H4SB = Font.custom("SUIT-SemiBold", size: 16)
+
+    static let B1M = Font.custom("SUIT-Medium", size: 12)
+    static let B1R = Font.custom("SUIT-Regular", size: 12)
+    static let B1T = Font.custom("SUIT-Thin", size: 12)
+    
+    static let B2M = Font.custom("SUIT-Medium", size: 10)
+    static let B2R = Font.custom("SUIT-Regular", size: 10)
+    
+    static let B3SB = Font.custom("SUIT-SemiBold", size: 8)
+    static let B3M = Font.custom("SUIT-Medium", size: 8)
+    static let B3R = Font.custom("SUIT-Regular", size: 8)
+    
+    static let B4M = Font.custom("SUIT-Medium", size: 6)
+    static let B4R = Font.custom("SUIT-Regular", size: 6)
+}
--- LocalHoliday/LocalHoliday/Extension/Image+Extension.swift
+++ LocalHoliday/LocalHoliday/Extension/Image+Extension.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 extension Image {
+    // MARK: - Image
     static let 강원 = Image("강원")
     static let 경기 = Image("경기")
     static let 경남 = Image("경남")
@@ -17,4 +18,12 @@ extension Image {
     static let 전북 = Image("전북")
     static let 제주 = Image("제주")
     static let 충청 = Image("충청")
+    static let LocalHoliday = Image("LocalHoliday")
+
+    // MARK: - ICON
+    static let Home = Image("Home")
+    static let Person = Image("Person")
+    static let Reservation = Image("Reservation")
+    static let Search = Image("Search")
+    static let Star = Image("Star")
 }
